83d6bc527fd652c22225205a6b7b014d2fa8a017
moazam <moazamshahzad222@gmail.com>
2022-01-31 22:32:29 -0500

    replacing access and secretr keys

--- my_first_ec2.tf
+++ my_first_ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
    region     = "us-east-1"
-  access_key = "AKIAUUXZPR4S7JBKUUUW"
-  secret_key = "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"
+  access_key = "ACCESS_KEY"
+  secret_key = "SCERET_KEY"
 }
 resource "aws_instance" "My_first_ec2" {
 ami = "ami-04902260ca3d33422"
